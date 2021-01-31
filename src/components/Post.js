62b7c78ae0669958a9aeb08dab5b54afafcfda46
seema <seemagreat.911@gmail.com>
2021-02-01 02:07:31 +0500

    image fix

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,13 +3,13 @@ import marked from 'marked'
 
 const Post = ({ article }) => {
     console.log(article.fields,'text')
-    const { name, Featuredimage, description } = article.fields
+    const { name, featuredimage, description } = article.fields
     const postDescription = marked(description)
-    console.log(Featuredimage,'abc');
+    console.log(featuredimage,'abc');
     return (
         <div className='post'>
             <h2 className='title'>{name}</h2>
-            {Featuredimage && <img className='Featuredimage' src={Featuredimage.fields.file.url} alt={name} title={name} />}
+            {featuredimage && <img className='Featuredimage' src={featuredimage.fields.file.url} alt={name} title={name} />}
             <section dangerouslySetInnerHTML={{ __html: postDescription }} />
         </div>
     )
